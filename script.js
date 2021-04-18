fc76e69ef25d1ac03739875e2d4c01503b9685ac
Tequila <abemily93@gmail.com>
2021-04-17 21:58:50 -0500

    Changes

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+let scriptHelper = require("./scriptHelper.js");
 
+window.addEventListener("load", function() {
+   
+    let form = document.querySelector("testForm");
+    form.addEventListener("submit", function(event) {
+       let pilot = document.querySelector("input[name=pilotName]");
+       let copilot = document.querySelector("input[name=copilotName]");
+       let fuelLevel = document.querySelector("input[name=fuelLevel]");
+       let cargoMass = document.querySelector("input[name=cargoMass]");
+       let list = document.querySelector("input [name=faultyItems]");
+
+       if (pilot.value === "" || copilot.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+        alert("All fields are required!");
+        event.preventDefault();
+       }
+       scriptHelper.formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+    });
+    
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
@@ -13,4 +30,4 @@ window.addEventListener("load", function() {
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
    
-});
+}); 
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,11 +17,41 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+   if (!testImput) {
+       result = "Empty";
+   } else if (isNaN(testImput)) {
+       result = "Not a Number";
+   } else (!isNaN(testImput)) {
+       result = "Is a Number";
+   }
+   return result
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
    
+    let pilotStatus = document.querySelector("#pilotStatus");
+    pilotStatus.textContent = `${pilotName} Ready`;
+
+    let copilotStatus = document.querySelector("#copilotStatus");
+    copilotStatus.textContent = `${copilotName} Ready`;
+   
+    let launchStatus = document.querySelector("#launchStatus");
+    if (validateInput(fuelLevel.textContent) < 10000) {
+        faultyItems.visibilty = true;
+        faultyItems.textContent = "There is not enough fuel for the journey."
+        launchStatus.textContent = "Shuttle not ready for launch.";
+        launchStatus.class = red;
+    } if (validateInput(cargoMass.textContent) > 10000) {
+        faultyItems.visibility = true;
+        faultyItems.textContent = "There is too much mass for the shuttle to take off.";
+        launchStatus.textContent = "Shuttle not ready for launch.";
+        launchStatus.class = red;
+    } else {
+        faultyItems.visibility = false;
+        launchStatus.textContent = "Shuttle is ready for launch.";
+        launchStatus.class = green;
+    }
+
 }
 
 async function myFetch() {
