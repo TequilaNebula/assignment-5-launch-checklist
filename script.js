9c17199b1ca766f48da72a4fcac429cdcc476525
Tequila <abemily93@gmail.com>
2021-04-17 22:38:48 -0500

    work????

--- script.js
+++ script.js
@@ -23,12 +23,10 @@ window.addEventListener("load", function() {
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse = scriptHelper.myFetch();
    listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-       scriptHelper.pickPlanet(listedPlanetsResponse);
+       let planet = scriptHelper.pickPlanet(listedPlanetsResponse);
+       scriptHelper.addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet. distance, planet.moons, planet.image);
    })
    
 }); 
